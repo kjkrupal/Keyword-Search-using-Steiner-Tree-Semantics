5081c2fd5b47a7dacd89cb1b98ec27f15d62ff5e
kjkrupal <kjkrupal@gmail.com>
2018-05-03 19:27:45 -0700

    Improved search output

--- search.py
+++ search.py
@@ -6,15 +6,18 @@ source = open(filename,"r")
 contents = source.read()
 soup = BeautifulSoup(contents,'xml')
 keyword = raw_input("Enter the word to be searched: ")
-searching = soup.find_all(string=re.compile(keyword, re.IGNORECASE))
-list=[]
+searching = soup.find_all(string = re.compile(keyword, re.IGNORECASE))
+
 
 for word in searching:
     for x in word.parents:
         if x is None:
             print("end")
         else:
-            if x.name=="dblp":
-                print(word)
+            if x.name == "dblp":
+                for i in range(0, len(word.contents)):
+                    if i % 2 != 0:
+                        print(word.contents[i].name + " : " + word.contents[i].contents[0])
                 break
-            word=word.parent
+            word = word.parent
+            print("\n")
