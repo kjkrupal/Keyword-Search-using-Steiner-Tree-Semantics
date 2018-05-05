74c58130f97a61d769ee51ce315f3fbcc657fd04
kjkrupal <kjkrupal@gmail.com>
2018-05-04 23:57:19 -0700

    Files added

---
+++ README.md
@@ -0,0 +1,1 @@
+As the DBLP file was too large to add, a sample version of DBLP has been added.
---
+++ test.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import re
+
+filename = "Input/Sample DBLP.xml"
+source = open(filename,"r")
+contents = source.read()
+soup = BeautifulSoup(contents,'xml')
+keyword = raw_input("Enter the word to be searched: ")
+searching = soup.find_all(string = re.compile(keyword, re.IGNORECASE))
+
+
+for word in searching:
+    for x in word.parents:
+        if x is None:
+            print("end")
+        else:
+            if x.name == "dblp":
+                for i in range(0, len(word.contents)):
+                    if i % 2 != 0:
+                        print(word.contents[i].name + " : " + word.contents[i].contents[0])
+                break
+            word = word.parent
+            
